b5e4ec8a8936d89c23b2ed9b04160cfbfbe80592
huumai1903 <146205608+huumai1903@users.noreply.github.com>
2024-06-24 22:42:40 +0700

    Section 2
    Commit section 2
    Monolithic build-up

---
+++ section2/accounts/src/main/java/com/eazybytes/accounts/dto/CustomerDto.java
@@ -0,0 +1,11 @@
+package com.eazybytes.accounts.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data @AllArgsConstructor
+public class CustomerDto {
+    private String name;
+    private String email;
+    private String mobileNumber;
+}
